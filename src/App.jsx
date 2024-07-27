80bfbe1a2c594becda8cd1bd27506abd053f88ce
zeeh <956100390@qq.com>
2024-07-28 01:15:25 +0800

    hash 路由修改

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes,HashRouter } from 'react-router-dom'
 import OrderTable from './pages/orderTable/index'
-import Home from './pages/home/index';
 import NotFound from './pages/notFound/index';
 import addSettlement from './pages/addSettlement/index';
 import './App.css'
@@ -8,14 +7,13 @@ import './App.css'
 const App = () => {
   return (
     <>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route path="/order_table" element={OrderTable()} />
-          <Route path="/home" element={addSettlement()} />
-          <Route path="/add_settlement" element={addSettlement()} />
+          <Route path="/" element={addSettlement()} />
           <Route path="*" element={NotFound()} />
         </Routes>
-      </Router>
+      </HashRouter>
     </>
   )
 }
