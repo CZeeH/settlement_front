afd25c8c789f6743993ebdfcae0f73f353e0cef6
zeeh <956100390@qq.com>
2024-08-14 20:43:41 +0800

    重新打包

--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
-// const pathServer = 'http://47.99.132.17:3000' // 后端地址
-const pathServer = 'http://localhost:3000' // 后端地址
+const pathServer = 'http://47.99.132.17:3000' // 后端地址
+// const pathServer = 'http://localhost:3000' // 后端地址
 
 export {pathServer}
